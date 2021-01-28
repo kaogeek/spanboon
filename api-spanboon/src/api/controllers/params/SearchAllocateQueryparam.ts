610b85f1da946f45a5186b8fcd7714745b517102
Shiorin <junsuda.s@absolute.co.th>
2021-01-28 15:49:11 +0700

    Search Allocate API /api/allocate/search

---
+++ api-spanboon/src/api/controllers/params/SearchAllocateQueryparam.ts
@@ -0,0 +1,16 @@
+/*
+ * @license Spanboon Platform v0.1
+ * (c) 2020-2021 KaoGeek. http://kaogeek.dev
+ * License: MIT. https://opensource.org/licenses/MIT
+ * Author:  shiorin <junsuda.s@absolute.co.th>
+ */
+
+import 'reflect-metadata';
+
+export class SearchAllocateQueryparam {
+    public emergencyEvent: string;
+    public objective: string;
+    public orderBy: string;
+    public limit: number;
+    public offset: number;
+}
