0257eb286001c8c1b5a71b40d29aec93e6101af5
Ploy <nattawadee.l@absolute.co.th>
2021-03-10 12:29:54 +0700

    api add method account_verify

--- api-spanboon/src/api/controllers/PagePostController.ts
+++ api-spanboon/src/api/controllers/PagePostController.ts
@@ -1126,11 +1126,9 @@ export class PagePostController {
                         }
                     },
                     {
-                        $project: {
-                            'ownerUser._id': 0,
+                        $project: {  
                             'ownerUser.username': 0,
-                            'ownerUser.password': 0,
-                            'ownerUser.uniqueId': 0,
+                            'ownerUser.password': 0, 
                             'ownerUser.coverURL': 0,
                             'ownerUser.coverPosition': 0,
                             'ownerUser.isSubAdmin': 0,
