eed346429d99e53262cd73575414ea21c6828820
Shiorin <junsuda.s@absolute.co.th>
2021-05-07 08:22:42 +0700

    Emergency event Page API and fix loop bug @PostService

--- api-spanboon/src/api/services/EmergencyEventService.ts
+++ api-spanboon/src/api/services/EmergencyEventService.ts
@@ -11,11 +11,16 @@ import { EmergencyEvent } from '../models/EmergencyEvent';
 import { EmergencyEventRepository } from '../repositories/EmergencyEventRepository';
 import { SearchUtil } from '../../utils/SearchUtil';
 import { SearchFilter } from '../controllers/requests/SearchFilterRequest';
+import { FulfillmentCaseService } from '../services/FulfillmentCaseService';
+import { PostsService } from '../services/PostsService';
+import { ObjectID } from 'mongodb';
+import { POST_TYPE } from '../../constants/PostType';
 
 @Service()
 export class EmergencyEventService {
 
-    constructor(@OrmRepository() private emergencyEventRepository: EmergencyEventRepository) { }
+    constructor(@OrmRepository() private emergencyEventRepository: EmergencyEventRepository, private fulfillmentCaseService: FulfillmentCaseService,
+        private postsService: PostsService) { }
 
     // find EmergencyEvent
     public find(findCondition?: any): Promise<EmergencyEvent[]> {
@@ -56,5 +61,187 @@ export class EmergencyEventService {
         } else {
             return this.emergencyEventRepository.find(condition);
         }
+    }
+
+    public sampleFulfillmentUser(emergencyEventId: ObjectID, simpleCount: number, fullfilStatus?: string): Promise<any> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = {
+                    count: 0,
+                    fulfillmentUser: [],
+                    fulfillmentUserCount: 0
+                };
+
+                if (emergencyEventId === undefined || emergencyEventId === null) {
+                    resolve(result);
+                    return;
+                }
+
+                const matchStmt: any = {
+                    postId: { $exists: true }
+                };
+
+                if (fullfilStatus !== undefined) {
+                    matchStmt.status = fullfilStatus;
+                }
+
+                // count fullfilment case with page objective id
+                const aggregateCountObjPost = [
+                    { $match: matchStmt },
+                    {
+                        $lookup: {
+                            from: 'Posts',
+                            localField: 'postId',
+                            foreignField: '_id',
+                            as: 'posts'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$posts',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    { $match: { 'posts.emergencyEvent': emergencyEventId, 'posts.deleted': false } },
+                    { $group: { _id: null, count: { $sum: 1 } } },
+                    { $project: { _id: 0 } }
+                ];
+                const countAggResult = await this.fulfillmentCaseService.aggregate(aggregateCountObjPost);
+
+                if (countAggResult.length > 0) {
+                    result.count = countAggResult[0].count;
+                }
+
+                // search fulfillment case sample user
+                const aggregateObjPost = [
+                    { $match: matchStmt },
+                    {
+                        $lookup: {
+                            from: 'Posts',
+                            localField: 'postId',
+                            foreignField: '_id',
+                            as: 'posts'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$posts',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    { $match: { 'posts.emergencyEvent': emergencyEventId, 'posts.deleted': false } },
+                    { $sample: { size: simpleCount } },
+                    { $group: { _id: { requester: '$requester' }, count: { $sum: 1 } } },
+                    {
+                        $lookup: {
+                            from: 'User',
+                            localField: '_id.requester',
+                            foreignField: '_id',
+                            as: 'user'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$user',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    {
+                        $project: {
+                            'user.password': 0,
+                            'user.coverPosition': 0,
+                            'user.birthdate': 0,
+                            'user.gender': 0,
+                            'user.customGender': 0
+                        }
+                    }
+                ];
+
+                const aggResult = await this.fulfillmentCaseService.aggregate(aggregateObjPost);
+                for (const row of aggResult) {
+                    result.fulfillmentUser.push(row.user);
+                }
+
+                const aggregateCountAllUserPost = [
+                    { $match: matchStmt },
+                    {
+                        $lookup: {
+                            from: 'Posts',
+                            localField: 'postId',
+                            foreignField: '_id',
+                            as: 'posts'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$posts',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    { $match: { 'posts.emergencyEvent': emergencyEventId, 'posts.deleted': false } },
+                    { $group: { _id: { requester: '$requester' }, count: { $sum: 1 } } },
+                    { $group: { _id: { requester: '$_id.requester' }, count: { $sum: 1 } } }
+                ];
+
+                const countAllUserResult = await this.fulfillmentCaseService.aggregate(aggregateCountAllUserPost);
+                if (countAllUserResult.length > 0) {
+                    result.fulfillmentUserCount = countAllUserResult[0].count;
+                }
+
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    public sampleRelatedHashTags(emergencyEventId: ObjectID, simpleCount: number): Promise<any[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let result = [];
+
+                if (emergencyEventId === undefined || emergencyEventId === null) {
+                    resolve(result);
+                    return;
+                }
+
+                const matchStmt: any = {
+                    emergencyEvent: emergencyEventId,
+                    postsHashTags: { $exists: true, $type: 'objectId' },
+                    deleted: false
+                };
+
+                result = await this.postsService.sampleHashTags(matchStmt, simpleCount);
+
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    public sampleNeedsItems(emergencyEventId: ObjectID, simpleCount: number): Promise<any[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let result = [];
+
+                if (emergencyEventId === undefined || emergencyEventId === null) {
+                    resolve(result);
+                    return;
+                }
+
+                const matchStmt: any = {
+                    emergencyEvent: emergencyEventId,
+                    deleted: false,
+                    type: POST_TYPE.NEEDS
+                };
+
+                result = await this.postsService.samplePostNeedsItems(matchStmt, simpleCount);
+
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 }
