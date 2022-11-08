a7606f7a468a3b8b00bb6a3c4743f1381023986d
Nes224 <Nes224@hotmail.com>
2022-11-08 16:28:02 +0700

    Facebook_axios_post

--- api-spanboon/src/loaders/jobSchedulerLoader.ts
+++ api-spanboon/src/loaders/jobSchedulerLoader.ts
@@ -6,7 +6,6 @@
  */
 
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-import { env } from '../env';
 import * as schedule from 'node-schedule';
 import axios from 'axios';
 
@@ -17,7 +16,7 @@ export const jobSchedulerLoader: MicroframeworkLoader = (settings: Microframewor
     // Run Every Hour
     // Clear Temp File
     schedule.scheduleJob('*/5 * * * *', () => {
-        axios.delete(`http://${env.app.host}:${env.app.port}${env.app.routePrefix}/file/temp`).then((res) =>{
+        axios.post(process.env.APP_DELETE_TEMP).then((res) =>{
             console.log(`Clear Temp File : ${res.status}`);
         }).catch((err) =>{
             console.log('err: ' + err);
@@ -27,7 +26,7 @@ export const jobSchedulerLoader: MicroframeworkLoader = (settings: Microframewor
     // Run Every 3 Hour
     // update page token
     schedule.scheduleJob('0 */3 * * *', () => {
-        axios.post(`http://${env.app.host}:${env.app.port}${env.app.routePrefix}/jobs/extended_token`).then((res) =>{
+        axios.post(process.env.APP_UPDATE_TOKEN).then((res) =>{
             console.log(`update page token : ${res.status}`);
         }).catch((err) =>{
             console.log('err: ' + err);
@@ -36,7 +35,7 @@ export const jobSchedulerLoader: MicroframeworkLoader = (settings: Microframewor
     
     // fetch feed twitter
     schedule.scheduleJob('*/1 * * * *', () =>{
-        axios.get(`http://${env.app.host}:${env.app.port}${env.app.routePrefix}/twitter/feed_tw`).then((res)=>{
+        axios.get(process.env.APP_TWITTER).then((res)=>{
             console.log(`Fetch Twitter : ${res.status}`);
         }).catch((err)=>{
             console.log('err: ' + err);
