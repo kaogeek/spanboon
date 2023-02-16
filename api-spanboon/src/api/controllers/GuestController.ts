0a3418c164ac475e497557a84322b3bc0f2ba323
Nes224 <Nes224@hotmail.com>
2023-02-16 15:08:56 +0700

    Otp&&DeleteUser

--- api-spanboon/src/api/controllers/GuestController.ts
+++ api-spanboon/src/api/controllers/GuestController.ts
@@ -1680,6 +1680,7 @@ export class GuestController {
         let saveOtp = undefined;
         const emailRes: string = username;
         const expirationDate = moment().add(5, 'minutes').toDate().getTime();
+        const momentExpiration = moment().add(10, 'minutes').toDate().getTime();
         const user: User = await this.userService.findOne({ username: emailRes });
         const checkOtp = await this.otpService.findOne({ userId: ObjectID(user.id), email: user.email });
         if (checkOtp !== undefined && checkOtp.expiration > expirationDate) {
@@ -1716,7 +1717,7 @@ export class GuestController {
             // const sendMailRes = await this.sendActivateOTP(user, emailRes, limitCount.otp, 'Send OTP');
             const successResponse = ResponseUtil.getSuccessOTP('The Otp have been send.', limitCount.limit);
             return res.status(200).send(successResponse);
-        } else if(limitCount.limit === 3 && limitCount.expiration >expirationDate){
+        } else if(limitCount.limit === 3 && limitCount.expiration > momentExpiration ){
             const query = { email: emailRes };
             await this.otpService.delete(query);
             return res.status(400).send(ResponseUtil.getErrorResponse('The Otp have been send more than 3 times And expiration OTP.', undefined));
