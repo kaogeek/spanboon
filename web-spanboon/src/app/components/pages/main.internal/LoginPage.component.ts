e6caeda2a8f9f9bb2dcca1b80e0724a36c78f062
bugtiv <bugtiv@gmail.com>
2023-02-21 13:24:01 +0700

    login google redirect register

--- web-spanboon/src/app/components/pages/main.internal/LoginPage.component.ts
+++ web-spanboon/src/app/components/pages/main.internal/LoginPage.component.ts
@@ -462,7 +462,17 @@ export class LoginPage extends AbstractPage implements OnInit {
               queryParams: { mode: 'google' }
             }
             this.router.navigate(['/register'], navigationExtras);
-          } else if (err.error.message === 'Baned PageUser.') {
+          } else if (statusMsg === "This Email not exists") {
+            let navigationExtras: NavigationExtras = {
+              state: {
+                accessToken: this.accessToken,
+                redirection: this.redirection
+              },
+              queryParams: { mode: 'google' }
+            }
+            this.router.navigate(['/register'], navigationExtras);
+          }
+          else if (err.error.message === 'Baned PageUser.') {
             this.dialog.open(DialogAlert, {
               disableClose: true,
               data: {
