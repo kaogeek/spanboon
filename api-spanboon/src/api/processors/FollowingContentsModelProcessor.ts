dcb3ca13752119d1b20a78ec15b20f56be3b8b46
Nes224 <Nes224@hotmail.com>
2023-06-01 12:03:56 +0700

    commit

--- api-spanboon/src/api/processors/FollowingContentsModelProcessor.ts
+++ api-spanboon/src/api/processors/FollowingContentsModelProcessor.ts
@@ -78,10 +78,10 @@ export class FollowingContentsModelProcessor extends AbstractSeparateSectionProc
                         }
                     },
                     {
-                        $limit: limitFollows + offsetFollows
+                        $skip: offsetFollows
                     },
                     {
-                        $skip: offsetFollows
+                        $limit: limitFollows + offsetFollows
                     },
                     {
                         $project: {
@@ -147,11 +147,11 @@ export class FollowingContentsModelProcessor extends AbstractSeparateSectionProc
                                             },
                                         },
                                         {
-                                            $limit: limit + offset,
-
+                                            $skip: offset
                                         },
                                         {
-                                            $skip: offset
+                                            $limit: limit + offset,
+
                                         },
                                         {
                                             $lookup: {
@@ -231,11 +231,11 @@ export class FollowingContentsModelProcessor extends AbstractSeparateSectionProc
                                             },
                                         },
                                         {
-                                            $limit: limit + offset,
-
+                                            $skip: offset
                                         },
                                         {
-                                            $skip: offset
+                                            $limit: limit + offset,
+
                                         },
                                         {
                                             $lookup: {
@@ -313,11 +313,11 @@ export class FollowingContentsModelProcessor extends AbstractSeparateSectionProc
                                             },
                                         },
                                         {
-                                            $limit: limit + offset,
-
+                                            $skip: offset
                                         },
                                         {
-                                            $skip: offset
+                                            $limit: limit + offset,
+
                                         },
                                         {
                                             $lookup: {
@@ -386,11 +386,11 @@ export class FollowingContentsModelProcessor extends AbstractSeparateSectionProc
                                             },
                                         },
                                         {
-                                            $limit: limit + offset,
-
+                                            $skip: offset
                                         },
                                         {
-                                            $skip: offset
+                                            $limit: limit + offset,
+
                                         },
                                         {
                                             $lookup: {
