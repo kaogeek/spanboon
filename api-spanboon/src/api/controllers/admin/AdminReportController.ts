aa1a5f1f0f1f58fc44c5524d437c74b75059d0fb
Nes224 <Nes224@hotmail.com>
2023-06-28 14:26:27 +0700

    adminReport

--- api-spanboon/src/api/controllers/admin/AdminReportController.ts
+++ api-spanboon/src/api/controllers/admin/AdminReportController.ts
@@ -1,14 +1,15 @@
 import { JsonController, Res, Post, Req, Authorized, Param } from 'routing-controllers';
 import { ResponseUtil } from '../../../utils/ResponseUtil';
-import { UserBlockContentService } from '../../services/UserBlockContentService';
+import { UserReportContentService } from '../../services/UserReportContentService';
 import { ObjectID } from 'mongodb';
 import { PageService } from '../../services/PageService';
 import { UserService } from '../../services/UserService';
 import { PostsService } from '../../services/PostsService';
+import { MAILService } from '../../../auth/mail.services';
 @JsonController('/admin/report')
 export class AdminReportController {
     constructor(
-        private userBlockContentService: UserBlockContentService,
+        private userReportContentService: UserReportContentService,
         private pageService: PageService,
         private userService: UserService,
         private postsService: PostsService
@@ -17,66 +18,109 @@ export class AdminReportController {
     @Post('/')
     @Authorized()
     public async searchReport(@Res() res: any, @Req() req: any): Promise<any> {
-        const searchReported = await this.userBlockContentService.aggregate(
+        const pageReport = await this.pageService.aggregate(
             [
                 {
                     $lookup: {
-                        from: 'Posts',
-                        let: { subjectId: '$subjectId' },
+                        from: 'UserReportContent',
+                        let: { id: '$_id' },
                         pipeline: [
                             {
                                 $match: {
                                     $expr: {
-                                        $eq: ['$$subjectId', '$_id']
+                                        $eq: ['$$id', '$typeId']
                                     }
                                 }
                             }
                         ],
-                        as: 'posts'
+                        as: 'userReportContent'
                     }
                 },
                 {
+                    $match: {
+                        userReportContent: { $ne: [] }
+                    }
+                }
+            ]
+        );
+        const userReport = await this.userService.aggregate(
+            [
+                {
                     $lookup: {
-                        from: 'User',
-                        let: { subjectId: '$subjectId' },
+                        from: 'UserReportContent',
+                        let: { id: '$_id' },
                         pipeline: [
                             {
                                 $match: {
                                     $expr: {
-                                        $eq: ['$$subjectId', '$_id']
+                                        $eq: ['$$id', '$typeId']
                                     }
                                 }
                             }
                         ],
-                        as: 'user'
+                        as: 'userReportContent'
                     }
                 },
                 {
+                    $match: {
+                        userReportContent: { $ne: [] }
+                    }
+                }
+            ]
+        );
+        const postReport = await this.postsService.aggregate(
+            [
+                {
                     $lookup: {
-                        from: 'Page',
-                        let: { subjectId: '$subjectId' },
+                        from: 'UserReportContent',
+                        let: { id: '$_id' },
                         pipeline: [
                             {
                                 $match: {
                                     $expr: {
-                                        $eq: ['$$subjectId', '$_id']
+                                        $eq: ['$$id', '$typeId']
                                     }
                                 }
                             }
                         ],
-                        as: 'page'
+                        as: 'userReportContent'
+                    }
+                },
+                {
+                    $match: {
+                        userReportContent: { $ne: [] }
                     }
                 }
             ]
         );
         const result: any = [];
-        result.post = undefined;
-        result.title = undefined;
-        result.detail = undefined;
-        result.type = undefined;
-        result.count = searchReported.length;
-        if (result) {
-            const successResponseGroup = ResponseUtil.getSuccessResponse('Search report is successful.', result);
+        for (let i = 0; i < pageReport.length; i++) {
+            const reports: any = {};
+            reports.page = pageReport[i].name;
+            reports.type = 'PAGE';
+            reports.count = await this.countReport(pageReport[i], reports.type);
+            result.push(reports);
+        }
+        for (let j = 0; j < userReport.length; j++) {
+            const reports: any = {};
+            reports.page = userReport[j].displayName;
+            reports.type = 'USER';
+            reports.count = await this.countReport(userReport[j], reports.type);
+            result.push(reports);
+        }
+        for (let z = 0; z < postReport.length; z++) {
+            const reports: any = {};
+            reports.page = postReport[z].title;
+            reports.type = 'POST';
+            reports.count = await this.countReport(postReport[z], reports.type);
+            result.push(reports);
+        }
+        let sortCount = undefined;
+        if (result.length > 0) {
+            sortCount = result.sort((a, b) => b.count - a.count);
+        }
+        if (sortCount.length > 0) {
+            const successResponseGroup = ResponseUtil.getSuccessResponse('Search report is successful.', sortCount);
             return res.status(200).send(successResponseGroup);
         } else {
             const errorResponse = ResponseUtil.getErrorResponse('There are no reports yet.', undefined);
@@ -151,7 +195,7 @@ export class AdminReportController {
                     query = { _id: page.id };
                     newValues = { $set: { banned: ban } };
                     const update = await this.pageService.update(query, newValues);
-                    const reset = await this.userBlockContentService.deleteMany({ _id: objIds, subjectType: types });
+                    const reset = await this.userReportContentService.deleteMany({ _id: objIds, type: types });
                     if (update && reset) {
                         const successResponse = ResponseUtil.getSuccessResponse('Your approve is sucessful.', update);
                         return res.status(200).send(successResponse);
@@ -163,7 +207,7 @@ export class AdminReportController {
                     query = { _id: user.id };
                     newValues = { $set: { banned: ban } };
                     const update = await this.userService.update(query, newValues);
-                    const reset = await this.userBlockContentService.deleteMany({ _id: objIds, subjectType: types });
+                    const reset = await this.userReportContentService.deleteMany({ _id: objIds, type: types });
                     if (update && reset) {
                         const successResponse = ResponseUtil.getSuccessResponse('Your approve is sucessful.', update);
                         return res.status(200).send(successResponse);
@@ -175,7 +219,7 @@ export class AdminReportController {
                     query = { _id: post.id };
                     newValues = { $set: { banned: ban } };
                     const update = await this.postsService.update(query, newValues);
-                    const reset = await this.userBlockContentService.deleteMany({ _id: objIds, subjectType: types });
+                    const reset = await this.userReportContentService.deleteMany({ _id: objIds, type: types });
                     if (update && reset) {
                         const successResponse = ResponseUtil.getSuccessResponse('Your approve is sucessful.', update);
                         return res.status(200).send(successResponse);
@@ -188,6 +232,27 @@ export class AdminReportController {
         } else {
             const errorResponse = ResponseUtil.getErrorResponse('Type is null or undefined.', undefined);
             return res.status(400).send(errorResponse);
+        }
+    }
+
+    private async countReport(countLength: any, type: string): Promise<any> {
+        if (countLength.userReportContent.length === 1000) {
+            const email = 'example@hotmail.com';
+            const type_report = type;
+            await this.sendReportToAdmin(email, type_report, 'Report ละเมิดการใช้งาน platform');
+        }
+        return countLength.userReportContent.length;
+    }
+
+    private async sendReportToAdmin(email: string, type: string, subject: string): Promise<any> {
+        const message = '<p> มีการ Report ของ' + type + 'อาจจะมีการละเมิดกฏการใช้งานของ Platform </p>';
+
+        const sendMail = MAILService.pushNotification(message, email, subject);
+
+        if (sendMail) {
+            return ResponseUtil.getSuccessResponse('Your report has been sent to your email inbox.', '');
+        } else {
+            return ResponseUtil.getErrorResponse('error in sending email', '');
         }
     }
 }
