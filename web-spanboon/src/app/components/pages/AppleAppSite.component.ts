a1286015cb272f6afe4224da0d7746f20c1cff40
bugtiv <bugtiv@gmail.com>
2024-01-19 16:50:06 +0700

    voteEvent

--- web-spanboon/src/app/components/pages/AppleAppSite.component.ts
+++ web-spanboon/src/app/components/pages/AppleAppSite.component.ts
@@ -5,6 +5,7 @@ import { AuthenManager } from 'src/app/services/AuthenManager.service';
 import * as jsonData from '../../../.well-known/assetlinks.json';
 import { environment } from 'src/environments/environment';
 import { AbstractPage } from './AbstractPage';
+import { HttpClient } from '@angular/common/http';
 
 const PAGE_NAME: string = '.well-known';
 const PAGE_NAME_APPLE: string = '/apple-app-site-association';
@@ -18,9 +19,12 @@ export class AppleAppSite extends AbstractPage implements OnInit {
     public static readonly PAGE_NAME: string = PAGE_NAME;
     public static readonly PAGE_NAME_APPLE: string = PAGE_NAME_APPLE;
 
+    public respone: any;
+    public isAndroid: boolean = false;
     data: any = jsonData;
     constructor(
         router: Router,
+        private http: HttpClient,
         authenManager: AuthenManager,
         dialog: MatDialog) {
         super(PAGE_NAME, authenManager, dialog, router);
@@ -35,8 +39,11 @@ export class AppleAppSite extends AbstractPage implements OnInit {
 
         json[0].target.sha256_cert_fingerprints = newFingerprints;
         if (this.router.url === '/.well-known/apple-app-site-association') {
-            window.open(environment.apple_app_link);
-            this.router.navigateByUrl('/home');
+            // window.open(environment.apple_app_link);
+            // this.router.navigateByUrl(environment.apple_app_link);
+            this.getAppleAppSiteAssociation();
+        } else {
+            this.isAndroid = true;
         }
     }
 
@@ -48,5 +55,16 @@ export class AppleAppSite extends AbstractPage implements OnInit {
     }
     onDirtyDialogCancelButtonClick(): EventEmitter<any> {
         throw new Error('Method not implemented.');
+    }
+
+    public getAppleAppSiteAssociation(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let url: string = environment.apiBaseURL + '/voting/.well-known/apple-app-site-association/';
+            this.http.get(url).toPromise().then((response: any) => {
+                this.respone = response;
+            }).catch((error: any) => {
+                reject(error);
+            });
+        });
     }
 }
