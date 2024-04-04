44b0edc67344600363093d36446494c32ca91b02
nes224 <nes224@hotmail.com>
2024-04-04 11:22:30 +0700

    commit

--- api-spanboon/src/api/controllers/PointMfpController.ts
+++ api-spanboon/src/api/controllers/PointMfpController.ts
@@ -393,6 +393,11 @@ export class NotificationController {
                                 }
                             },
                             {
+                                $sort:{
+                                    createdDate:-1
+                                }
+                            },
+                            {
                                 $skip: skips
                             },
                             {
@@ -420,6 +425,11 @@ export class NotificationController {
                                 }
                             },
                             {
+                                $sort:{
+                                    createdDate:-1
+                                }
+                            },
+                            {
                                 $skip: skips
                             },
                             {
@@ -444,6 +454,11 @@ export class NotificationController {
                             {
                                 $match:{
                                     type:'REDEEM'
+                                }
+                            },
+                            {
+                                $sort:{
+                                    createdDate:-1
                                 }
                             },
                             {
