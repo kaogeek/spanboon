015fde3cf0d624fa93d372d9406d711d297a53e9
nes224 <nes224@hotmail.com>
2024-06-12 13:33:31 +0700

    sort selfOrder

---
+++ api-spanboon/src/api/controllers/admin/requests/AdminUpdateUserRequest.ts
@@ -0,0 +1,14 @@
+/*
+ * @license Spanboon Platform v0.1
+ * (c) 2020-2021 KaoGeek. http://kaogeek.dev
+ * License: MIT. https://opensource.org/licenses/MIT
+ * Author:  shiorin <junsuda.s@absolute.co.th>, chalucks <chaluck.s@absolute.co.th>
+ */
+
+import { IsString,IsNotEmpty } from 'class-validator';
+
+export class UpdateUser {
+    @IsNotEmpty()
+    @IsString()
+    public role: string;
+}
